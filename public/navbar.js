d398aec14bfe5f6e8f239d8a8c711cadee862073
munshon-uofa <munshon@arizona.edu>
2026-03-18 17:50:53 -0700

    Add Comments for Clarification

--- public/navbar.js
+++ public/navbar.js
@@ -11,7 +11,7 @@ function renderNavbar() {
     const links = [
         { name: 'Calendar', href:'calendar.html'}
     ];
-
+    // Different Nav Bars based on admin vs user
     if (currentUser.role === 'admin') {
         links.push({ name: 'Approvals', href: 'admin-requests.html'});
         links.push({ name: 'Staff Management', href: '#'});
@@ -21,17 +21,20 @@ function renderNavbar() {
         links.push({ name: 'Request Change', href: 'request-form.html' });
     }
 
+    // Add links to nav bar 
     links.forEach(link => {
         const li = document.createElement('li');
         li.innerHTML = `<a href="${link.href}">${link.name}</a>`;
         navLinks.appendChild(li);
     });
 
+    // Add toggle theme to nav bar
     const themeLi = document.createElement('li');
     themeLi.innerHTML = `<button class="nav-link-style" onclick="toggleTheme()">Theme</button>`;
     navLinks.appendChild(themeLi);
 }
 
+// Theme toggle
 function toggleTheme() {
     const html = document.documentElement;
     const currentTheme = html.getAttribute('data-theme');
@@ -39,6 +42,7 @@ function toggleTheme() {
     html.setAttribute('data-theme', newTheme);
 }
 
+// TEMP FUNC: Switch between admin and employee for now
 function switchRoleTest() {
     currentUser.role = currentUser.role === 'admin' ? 'employee' : 'admin';
     alert(`Switched to: ${currentUser.role}`);
