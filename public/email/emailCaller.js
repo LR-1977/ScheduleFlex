b3f0b9ffb2903dd7fe731fc77ef0773ca577e456
alexstu1 <158611991+alexstu1@users.noreply.github.com>
2026-04-26 09:03:55 -0700

    Made mongodb calls to send emails

--- public/email/emailCaller.js
+++ public/email/emailCaller.js
@@ -1,7 +1,11 @@
 /* The idea for this is that the calls for looking for the emails will run from here and
    we will use windows scheduler or Cron/systemctl to automatically run this code
 */
+const { MongoClient, ObjectId } = require("mongodb");
 
+const mongoUrl = "mongodb://127.0.0.1:27017/";
+const client = new MongoClient(mongoUrl);
+let db, usersColl, invitationsColl, eventsColl, requestsColl;
 
 const EMAIL_SERVER_URL = "http://<servername>:8025/send-email";
 const SERVER_TOKEN     = "superSecret";
@@ -31,7 +35,7 @@ async function sendEmail(data) {
     }
 }
 
-async function managerEmail(email, body, date, name, employees) {
+async function managerEmail(email, date, employees) {
     const emailBody = `Greetings ${name},\n` +
                 `Your upcoming shift on ${date} will have the following employees:\n` +
                 `${employees.join("\n")}`
@@ -44,8 +48,8 @@ async function managerEmail(email, body, date, name, employees) {
 }
 
 
-async function driverEmail(email, body, date, name) {
-    const emailBody = `Greetings ${name},\n` +
+async function driverEmail(email, date) {
+    const emailBody = `Greetings,\n` +
                 `This is an automated notification about your upcoming shift on ${date}`
     const payload = {
         "to": email,
@@ -54,3 +58,80 @@ async function driverEmail(email, body, date, name) {
     };
     sendEmail(payload)
 }
+
+
+
+
+
+async function mongoConnect() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+        db = client.db("schedFlexDB");
+        usersColl = db.collection("userCollection");
+        invitationsColl = db.collection("invitationCollection");
+        eventsColl = db.collection("eventCollection");
+        requestsColl = db.collection("requestCollection");
+        return true;
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        return false;
+    }
+}
+
+function sendUserNotifications() {
+    const day = new Date().getDate() + 1;
+    let event = await eventsColl.find({"day":day});
+
+    //create manager daily shift schedule
+    let employees = [];
+    for (let shift of event) {
+        let date = " "+ shift.start +"-" +shift.stop;
+        for (let employee of shift.asignedUsers) {
+            employees.push(" "+employee +":"+ date);
+        }
+    }
+
+
+
+    //send emails
+    for (let shift of event) {
+        for (let employee of shift.assignedUsers) {
+            const email = employee;
+            const date = shift.day+" "+shift.monthYear + " " + shift.day +" "+ shift.start +"-" +shift.stop;
+
+
+            let user = await usersColl.findOne({"email":employee});
+            if (!user){
+                continue;
+            }
+            if (user.role == "user") {
+                driverEmail(email,date);
+            } else {
+                managerEmail(email,date,employees);
+            }
+            
+        }
+    }
+
+}
+
+function sendManagerNotifications() {
+    const day = new Date().getDate() + 1;
+    let event = await eventsColl.find({"day":day});
+    
+}
+//below gets ran on the daily run of the file
+function main() {
+    if (!await mongoConnect()){
+        return;
+    }
+    sendUserNotifications();
+
+
+
+}
+main();
+
+
